c62d6aaa10537e3ee592bed38cd405b411c7ef6d
Trinh Xuan Tam <mail@trinhxuantam.com>
2024-05-08 11:46:57 +0200

    update cors policy

--- src/config.py
+++ src/config.py
@@ -6,5 +6,10 @@ class DBConfig(BaseSettings):
     DATABASE_URL: PostgresDsn
     DATABASE_ENCRYPTION_KEY: str
 
+class CorsConfig(BaseSettings):
+    CORS_ORIGINS: list[str]
+    CORS_HEADERS: list[str]
+    CORS_METHODS: list[str]
 
 db_config: DBConfig = DBConfig()
+cors_config: CorsConfig = CorsConfig()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+from starlette.middleware.cors import CORSMiddleware
 from src.utils.db_utils import close_db
 from src.modules.auth.router import router as auth_router
 from src.modules.weight.router import router as weight_router
+from src.config import cors_config
 
 
 @asynccontextmanager
@@ -33,4 +34,12 @@ app.include_router(
     weight_router,
     prefix="/weight",
     tags=["Weight tracking"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_config.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=cors_config.CORS_METHODS,
+    allow_headers=cors_config.CORS_HEADERS,
 )
